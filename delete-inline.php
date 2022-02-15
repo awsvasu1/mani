0bb457b4063403872694ddd2f345994db323e01b
awsvasu1 <95370095+awsvasu1@users.noreply.github.com>
2022-02-15 10:56:42 +0530

    Update delete-inline.php

--- delete-inline.php
+++ delete-inline.php
@@ -5,7 +5,7 @@ $stu_id = $_GET['id'];
 include 'config.php';
 
 $sql = "DELETE FROM student WHERE sid = {$stu_id}";
-$result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+$result = mysqli_query($conn, $sql) or die("Query is Unsuccessful.");
 
 header("Location: http://localhost/crud/index.php");
 
